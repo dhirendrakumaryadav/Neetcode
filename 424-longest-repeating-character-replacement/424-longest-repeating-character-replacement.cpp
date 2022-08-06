e8a90e44b3a18f3f0a14b830b059225fc088d61c
dhirendrakumaryadav <85394847+dhirendrakumaryadav@users.noreply.github.com>
2022-08-06 18:28:27 +0530

    Time: 0 ms (100.00%), Space: 7 MB (85.28%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,44 +2,11 @@ class Solution {
 public:
     
         int characterReplacement(string s, int k) {
-int n= s.size(),res=0;
-int l=0,r=0,maxf=0,count[26]={0};
 
-    while(r<n)
-    {
-        count[s[r]-'A']++;
-        maxf = max(maxf,count[s[r]-'A']);
-        
-        if((r-l+1) - maxf <= k) //check for allowed change in sub string
-        {
-            res = max(res,(r-l+1));//store curr max of length of window size
-            r++;
-        }else{
-            count[s[l]-'A']--;//decrement count of 1st letter
-            l++; //move window
-            r++;//here too
-        }
-    }
     
-    return res;
+    
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*int m[26]={0};
+        int m[26]={0};
         int l=0;
         int r=0;
         int n=s.size();
@@ -47,9 +14,9 @@ int l=0,r=0,maxf=0,count[26]={0};
         int res=0;
         while(r<n){
             m[s[r]-'A']++;
-            int mr=max(mr,m[s[r]-'A']);
+            mr=max(mr,m[s[r]-'A']);
             if(((r-l+1)-mr)<=k){
-                res=max(res,r-l+1);
+                res=max(res,(r-l+1));
                 r++;
             }
             else{
@@ -59,6 +26,6 @@ int l=0,r=0,maxf=0,count[26]={0};
             }
             
         }
-        return res;*/
+        return res;
     }
 };
