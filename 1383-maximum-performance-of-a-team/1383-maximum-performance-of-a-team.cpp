a82bbdd349e9a5e46df8f63a3cf587798e948770
dhirendrakumaryadav <85394847+dhirendrakumaryadav@users.noreply.github.com>
2022-09-11 10:20:34 +0530

    Time: 176 ms (27.11%), Space: 35.4 MB (88.03%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-       vector<pair<int, int>> ess(n);
-        for (int i = 0; i < n; ++i)
-            ess[i] = {efficiency[i], speed[i]};
-        sort(rbegin(ess), rend(ess));
-        long sumS = 0, res = 0;
-        priority_queue <int, vector<int>, greater<int>> pq; //min heap
-        for(auto& [e, s]: ess){
+        vector<pair<int,int>>v(n);
+        for(int i=0;i<n;i++){
+          v[i]={efficiency[i],speed[i]};
+        }
+        sort(rbegin(v),rend(v));
+         priority_queue <int, vector<int>, greater<int>> pq;
+       long sum=0;
+        long res=0;
+        for(auto& [e,s]:v){
             pq.emplace(s);
-            sumS += s;
-            if (pq.size() > k) {
-                sumS -= pq.top();
+            sum+=s;
+            if(pq.size()>k){
+                sum-=pq.top();
                 pq.pop();
             }
-            res = max(res, sumS * e);
+            res=max(res,sum*e);
         }
-        return res % (int)(1e9+7);
+      return res % (int)(1e9+7);
     }
 };
