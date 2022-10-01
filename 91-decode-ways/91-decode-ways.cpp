7191040ffd9365b9b5a6ec23f4861db5faf087d8
dhirendrakumaryadav <85394847+dhirendrakumaryadav@users.noreply.github.com>
2022-10-02 03:37:28 +0530

    Time: 3 ms (63.58%), Space: 6.1 MB (75.56%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n=s.size();
-        vector<int>dp(n+1,0);
-        if(s[0]=='0'||s.size()==0)return 0;
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-           if(s[i-1]>='1'&&s[i-1]<='9')
-                dp[i]=dp[i-1];
+    
+    int dp[102];
+    int decode(string& s, int idx, int n)
+    {
+        if(idx < n && s[idx] == '0') return 0;
+        if(idx >= n)
+            return 1;
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int ways = 0;
+        
+        // Pick single
+        if(s[idx] != '0') ways = decode(s, idx+1, n);
             
-            if(s[i-2]=='1'){
-                dp[i]+=dp[i-2];
-            }
-            else if(s[i-2]=='2'&&(s[i-1]>='0'&&s[i-1]<='6')){
-                dp[i]+=dp[i-2];
-            }
-        }
-        return dp[n];
+        // Pick couple
+        if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
+           ways += decode(s, idx+2, n);
+           
+        return dp[idx] = ways;
+    }
+    
+    int numDecodings(string s) {
+       
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        return decode(s, 0, n);
     }
 };
