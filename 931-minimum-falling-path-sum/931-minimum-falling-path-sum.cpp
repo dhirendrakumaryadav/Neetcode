a28f51f5daba40bd1b2d58f60ffccad73694ef70
dhirendrakumaryadav <85394847+dhirendrakumaryadav@users.noreply.github.com>
2022-10-05 22:51:25 +0530

    Time: 40 ms (14.07%), Space: 10.3 MB (26.50%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int get(vector<vector<int>>& matrix,int sr,int sc,int n,vector<vector<int>>&dp){
-        if(sr==n-1&&(sc>=0)&&sc<n)return matrix[sr][sc];
+        if(sr==n)return 0;
       if((sc<0||sc>=n))return INT_MAX;
-        if(sr==n-1&&(sc>=0)&&sc<n)return 0;
+       
         if(dp[sr][sc]!=-1)return dp[sr][sc];
         return dp[sr][sc]=matrix[sr][sc]+min((get(matrix,sr+1,sc-1,n,dp)),min(get(matrix,sr+1,sc,n,dp),get(matrix,sr+1,sc+1,n,dp)));
         
